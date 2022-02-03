af48e5f8354aabfc6f34b4d1db5954aa03c9e29a
Stephanie <stephpechthalt@aol.com>
2022-02-03 11:41:12 -0500

    added footer and linked to GitHub

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This webiste was coded by Stephanie Davis and is{" "}
+          <a href="https://github.com/spechthalt/react-weather">
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
