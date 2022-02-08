242d47c51df1659a2397a111288dbda0122f267a
Stephanie <stephpechthalt@aol.com>
2022-02-08 11:14:55 -0500

    added unit conversion

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("fahrenheit");
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function celsius() {
+    return Math.round(((props.fahrenheit - 32) * 5) / 9);
+  }
+
+  if (unit === "fahrenheit") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{props.fahrenheit}</span>
+        <span className="unit">
+          °F |{" "}
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{celsius()}</span>
+        <span className="unit">
+          {" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>{" "}
+          | °C
+        </span>
+      </span>
+    );
+  }
+}
