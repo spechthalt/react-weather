8fa8c0d6d7d451aa89e007b4de183f8f72fc2239
Stephanie <stephpechthalt@aol.com>
2022-03-08 17:29:48 -0800

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
   return (
     <span className="WeatherTemperature">
       <span className="temperature">{props.fahrenheit}</span>
